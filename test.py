3b143f7032fc1fefb12aece68eaae65a9b697af3
James <james@users.noreply.github.com>
2020-05-11 01:43:55 -0400

    Added timer and test function

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import timeit
 
 def label(func):
     return func
@@ -11,5 +12,20 @@ helper = label
 def test_function(n):
     return n
 
-
 print(test_function("Hello World"))
+
+
+def timer(func):
+    def inner(n):
+        start = timeit.timeit()
+        func(n)
+        end = timeit.timeit()
+        return f"{func.__name__}: {end-start}s"
+    return inner
+
+
+@timer
+def test_function_2(n):
+    return n*2
+
+print(test_function_2(10))
